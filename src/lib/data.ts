9a362f97fdbd8f4bdc0f1146efc4839f159952d7
Vladimir Torres Vargas <95933904+vtorres65@users.noreply.github.com>
2025-06-13 11:26:14 -0500

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,9 +1,9 @@
 export const personalInfo = {
   name: "Rishikesh S",
   location: "Coimbatore, India",
-  email: "rkesh2003@gmail.com",
-  github: "https://github.com/rishikesh2003",
-  linkedin: "https://www.linkedin.com/in/rishikeshs/",
+  email: "",
+  github: "https://github.com/",
+  linkedin: "https://www.linkedin.com/in/",
 };
 
 export const workExperience = [
