8e04a3cb82b740857fb076a3cee0cbf8a1cf3801
Vladimir Torres Vargas <95933904+vtorres65@users.noreply.github.com>
2025-06-14 17:23:31 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vtorres65.github.io',
+  site: 'https://vtorres65.github.io/vtorres65.portafolio/',
   vite: {
       plugins: [tailwindcss()],
   },
